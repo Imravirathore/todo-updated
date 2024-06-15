9e6c1af35f1e87153674626b2d4916e1977a44c4
Imravirathore <ravirathore672@gmail.com>
2024-06-15 23:08:46 +0530

    todo app interview question

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+  const [text, setText] = React.useState<string>('')
+  const [todoList, setTodoList] = React.useState<string[]>([])
+
+  // Add ToDo
+  const addTodo = () => {
+    setTodoList([...todoList, text])
+    setText('')
+  }
+
+// Delete ToDo
+  const deleteTodo = (index:number) => {
+    const newList = [...todoList];
+    newList.splice(index, 1); // Remove one element at 'index'
+    setTodoList(newList);
+  }
+
+  // Edit ToDo
+  const editTodo = (val:string) => {
+    console.log('val', val)
+    setText(val)
+
+  }
+
+  // Update ToDo
+  const updateTodo = (index:number) => {
+    const newList = [...todoList]
+    newList[index] = text
+    setTodoList(newList);
+    setText('')
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Hello ToDo App</h1>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+        <ul>
+          {
+            todoList && todoList.map((val, index) => {
+              return (
+                <>
+                  <li>{val}</li>
+                  <button style={{ cursor: 'pointer', backgroundColor: 'red' }} onClick={() => deleteTodo(index)}>Delete</button>
+                  <button style={{ cursor: 'pointer', backgroundColor: 'yellow' }} onClick={() => editTodo(val)}>Edit</button>
+                  <button style={{ cursor: 'pointer', backgroundColor: 'green' }} onClick={() => updateTodo(index)}>Update</button>
+
+                </>
+              )
+            })
+          }
+
+        </ul>
+      </div>
+    </>
   );
 }
 
